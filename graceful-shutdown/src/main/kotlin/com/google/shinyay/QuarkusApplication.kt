3b15dd33e59651325c2abb12af3214633c266639
shinyay <shinya.com@gmail.com>
2020-10-15 22:31:12 +0900

    Add Thread sleep function

--- graceful-shutdown/src/main/kotlin/com/google/shinyay/QuarkusApplication.kt
+++ graceful-shutdown/src/main/kotlin/com/google/shinyay/QuarkusApplication.kt
@@ -5,10 +5,13 @@ import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
-@Path("/")
+@Path("/hello")
 class QuarkusApplication {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "hello"
+    fun hello(): String {
+        Thread.sleep(10000L)
+        return "hello"
+    }
 }
